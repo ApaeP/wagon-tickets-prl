871d5e3d6d1738137b47575614f56e3cff08f491
Paul Portier <paulportier@gmail.com>
2022-03-08 23:42:01 +0100

    clean

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
   def home
-    # Batch.all.each(&:fetch_tickets)
     @tickets = Ticket.includes(:student).all
   end
 end
--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,8 +1,3 @@
-class ApplicationJob# < ActiveJob::Base
+class ApplicationJob
   include Sidekiq::Worker
-  # Automatically retry jobs that encountered a deadlock
-  # retry_on ActiveRecord::Deadlocked
-
-  # Most jobs are safe to ignore if the underlying records are no longer available
-  # discard_on ActiveJob::DeserializationError
 end
